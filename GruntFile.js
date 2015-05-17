4d105847a619b47c8c1b37a79c8068bf62a42e76
Andreas Otten <aotten77@googlemail.com>
2015-05-17 20:32:56 +0200

    add SCSS and and describer-config

---
+++ GruntFile.js
@@ -0,0 +1,523 @@
+/**
+ * Created by aotten77 on 31.03.2014.
+ */
+module.exports = function(grunt){
+
+	var referencefile = '/js/describer-config/example-referencefile.html';
+
+	var getObjFor = function(path, searchpath){
+		return {
+			from: new RegExp("\'"+ path.replace(/\//g, '\\/') +"[^/\\n]*\'", "g"),
+			// string replacement
+			to: function(matchedWord, index, fullText, regexMatches){
+				var _filename = '';
+				var _searchpath = searchpath || '';
+				grunt.file.recurse(_searchpath+path, callback);
+				function callback(abspath, rootdir, subdir, filename) {
+					var str = filename;
+					var patt = new RegExp('^'+matchedWord.replace(/\'/g, "").replace(path, "")+'\\.(.*)');
+					var res = patt.test(str);
+					if(res && (subdir == undefined)){
+						str = str.substring(0, str.length - ((str.substr(str.length - 3) === '.js') ? 3 : 4));
+						_filename = str;
+					}
+				}
+				if(_filename === ''){
+					return "\'empty\'";
+				}else{
+					return "\'"+ path + _filename +"\'";
+				}
+			}
+		};
+	};
+	// here we want to preserver the BOM of the layout file, otherwise all of the umlauts will die
+	grunt.file.defaultEncoding = 'utf8';
+	grunt.file.preserveBOM = true;
+
+	// These plugins provide necessary tasks.
+
+	grunt.loadNpmTasks('grunt-asset-revisions');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-text-replace');
+	grunt.loadNpmTasks('grunt-imageoptim');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bake');
+	grunt.loadNpmTasks('grunt-libsass');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	//grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	/*grunt.loadNpmTasks("grunt-remove-logging");
+	grunt.loadNpmTasks('grunt-dalek');
+	grunt.loadNpmTasks('grunt-webp');*/
+
+	grunt.registerTask('task_step1', function(){
+		grunt.initConfig({
+			clean: {
+				clean: ["css/build/", "js/build/"]
+			},
+			uglify: {
+				build : {
+					src     : ['**/*.js', '!*.min.js', '!**/main.js'],
+					cwd     : 'js/',
+					dest    : 'js/build/',
+					expand  : true
+				}
+			},
+			replace: {
+				example: {
+					src: ['js/build/**/*.js'],
+					overwrite: true,                 // overwrite matched source files
+					replacements: [{
+						from: /^define(.*?)function/,          // string replacement
+						to: function(matchedWord, index, fullText, regexMatches){
+							var match;
+							var _return;
+							try{
+								match = /a.plugin\("(.*?)"/i.exec(fullText)[1];
+								match = match.replace(/\./g, '_');
+								_return = matchedWord + ' __MOD__' + match;
+							}catch(e){
+								_return = matchedWord;
+							}
+							return _return;
+						}
+					}]
+				}
+			},
+			copy: {
+				main: {
+					src: ['**/*.eot', '**/*.svg', '**/*.ttf', '**/*.woff'],
+					cwd: 'js/',
+					dest: 'js/build/',
+					expand  : true
+				}
+			},
+			cssmin: {
+				compress: {
+					files: {
+						'css/build/style.css': ['css/style.css'],
+						'css/build/noscript.css': ['css/noscript.css'],
+						'js/build/describer-config/require-css/owl.carousel.css': ['js/describer-config/require-css/owl.carousel.css'],
+						'js/build/describer-config/require-css/video.css': ['js/describer-config/require-css/video.css']
+					}
+				}
+			},
+			revisions: {
+				rename: {
+					src: ['js/build/**', 'css/build/**', '!js/build/describer-more/empty.js', '!**/*.eot', '!**/*.svg', '!**/*.ttf', '!**/*.woff'],
+					dest: ''
+				}
+			}
+		});
+	});
+
+	grunt.registerTask('task_step2', function(){
+		grunt.initConfig({
+			replace: {
+				example: {
+					src: ['js/describer-config/main.js'],             // source files array (supports minimatch)
+					dest: 'js/build/describer-config/main.js',             // destination directory or file
+					replacements: [
+						{
+							from: 'main',
+							// string replacement
+							to: function(matchedWord, index, fullText, regexMatches){
+								var _filename = '';
+								grunt.file.recurse('js/build/describer-config/', callback);
+								function callback(abspath, rootdir, subdir, filename) {
+									var str = filename;
+									var patt = new RegExp('^'+matchedWord.replace(/\'/g, "")+'\\.(.*)');
+									var res = patt.test(str);
+									if(res && (subdir == undefined)){
+										str = str.substring(0, str.length - ((str.substr(str.length - 3) === '.js') ? 3 : 4));
+										_filename = str;
+									}
+								}
+								return _filename;
+							}
+						},
+
+						// describer-config
+						getObjFor('describer-config/', 'js/build/'),
+						getObjFor('describer-config/nls/', 'js/build/'),
+						getObjFor('describer-config/nls/en/', 'js/build/'),
+						getObjFor('describer-config/require-css/', 'js/build/'),
+
+						// describer-core
+						getObjFor('describer-core/', 'js/build/'),
+						getObjFor('describer-core/actions/', 'js/build/'),
+						getObjFor('describer-core/libs/', 'js/build/'),
+
+						// describer-localStorage
+						getObjFor('describer-localStorage/', 'js/build/'),
+
+						// describer-more
+						getObjFor('describer-more/modules/', 'js/build/'),
+						getObjFor('describer-more/modules/form/', 'js/build/'),
+						getObjFor('describer-more/plugins/', 'js/build/'),
+						getObjFor('describer-more/plugins/customized/', 'js/build/'),
+						getObjFor('describer-more/plugins/overwritings/', 'js/build/')
+					]
+				}
+			}
+		});
+	});
+	// Project configuration.
+	grunt.registerTask('task_step3', function(){
+		grunt.initConfig({
+			uglify: {
+				build : {
+					src     : ['js/build/describer-config/main.js'],
+					expand  : true
+				}
+			},
+			revisions: {
+				rename: {
+					src: 'js/build/describer-config/main.js',
+					dest: ''
+				}
+			}
+		});
+	});
+
+	grunt.registerTask('task_step4', function(){
+		grunt.initConfig({
+			replace: {
+				example: {
+					src: [referencefile],             // source files array (supports minimatch)
+					dest: referencefile,             // destination directory or file
+					replacements: [{
+						from: /css\/build\/style(.*?)\.css/g,          // string replacement
+						to: function(matchedWord, index, fullText, regexMatches){
+							var _filename = '';
+							grunt.file.recurse('css/build', callback);
+							function callback(abspath, rootdir, subdir, filename) {
+								var str = filename;
+								var patt = new RegExp(/^style.(.*)/g);
+								var res = patt.test(str);
+								if(res){
+									grunt.log.write('res: '+_filename);
+									_filename = str;
+								}
+
+							}
+							return "css/build/" + _filename;
+						}
+					},
+						{
+							from: /css\/build\/noscript(.*?)\.css/g,          // string replacement
+							to: function(matchedWord, index, fullText, regexMatches){
+								var _filename = '';
+								grunt.file.recurse('css/build', callback);
+								function callback(abspath, rootdir, subdir, filename) {
+									var str = filename;
+									var patt = new RegExp(/^noscript.(.*)/g);
+									var res = patt.test(str);
+									if(res){
+										grunt.log.write('res: '+_filename);
+										_filename = str;
+									}
+
+								}
+								return "css/build/" + _filename;
+							}
+						},
+						{
+							from: /js\/build\/describer-localStorage\/localStorage(.*?)\.js/g,                   // string replacement
+							to: function(matchedWord, index, fullText, regexMatches){
+								var _filename = '';
+								grunt.file.recurse('js/build/describer-localStorage/', callback);
+								function callback(abspath, rootdir, subdir, filename) {
+									var str = filename;
+									var patt = new RegExp(/^localStorage.(.*)/g);
+									var res = patt.test(str);
+									if(res){
+										//grunt.log.write('res: '+_filename);
+										_filename = str;
+									}
+
+								}
+								return "js/build/describer-localStorage/"+ _filename;
+							}
+						},
+						{
+							from: /js\/build\/describer-localStorage\/require.2.1.11-localStorage(.*?)\.js/g,                   // string replacement
+							to: function(matchedWord, index, fullText, regexMatches){
+								var _filename = '';
+								grunt.file.recurse('js/build/describer-localStorage/', callback);
+								function callback(abspath, rootdir, subdir, filename) {
+									var str = filename;
+									var patt = new RegExp(/^require.2.1.11-localStorage.(.*)/g);
+									var res = patt.test(str);
+									if(res){
+										//grunt.log.write('res: '+_filename);
+										_filename = str;
+									}
+
+								}
+								return "js/build/describer-localStorage/"+ _filename;
+							}
+						},
+						{
+							from: /js\/build\/describer-core\/libs\/modernizr.custom(.*?)\.js/g,                   // string replacement
+							to: function (matchedWord, index, fullText, regexMatches) {
+								var _filename = '';
+								grunt.file.recurse('js/build/describer-core/libs', callback);
+								function callback(abspath, rootdir, subdir, filename) {
+									var str = filename;
+									var patt = new RegExp(/^modernizr.custom.(.*)/g);
+									var res = patt.test(str);
+									if (res) {
+										//grunt.log.write('res: '+_filename);
+										_filename = str;
+									}
+
+								}
+								return "js/build/describer-core/libs/" + _filename;
+							}
+						},
+						{
+							from: /js\/build\/describer-core\/libs\/matchmedia(.*?)\.js/g,                   // string replacement
+							to: function (matchedWord, index, fullText, regexMatches) {
+								var _filename = '';
+								grunt.file.recurse('js/build/describer-core/libs', callback);
+								function callback(abspath, rootdir, subdir, filename) {
+									var str = filename;
+									var patt = new RegExp(/^matchmedia.(.*)/g);
+									var res = patt.test(str);
+									if (res) {
+										//grunt.log.write('res: '+_filename);
+										_filename = str;
+									}
+
+								}
+								return "js/build/describer-core/libs/" + _filename;
+							}
+						},
+						{
+							from: /js\/build\/describer-more\/plugins\/picturefill(.*?)\.js/g,                   // string replacement
+							to: function (matchedWord, index, fullText, regexMatches) {
+								var _filename = '';
+								grunt.file.recurse('js/build/describer-more/plugins', callback);
+								function callback(abspath, rootdir, subdir, filename) {
+									var str = filename;
+									var patt = new RegExp(/^picturefill.(.*)/g);
+									var res = patt.test(str);
+									if (res) {
+										//grunt.log.write('res: '+_filename);
+										_filename = str;
+									}
+
+								}
+								return "js/build/describer-more/plugins/" + _filename;
+							}
+						},
+						{
+							from: /\/js\/build\/describer-config\/main(.*)\'/g,                   // string replacement
+							to: function(matchedWord, index, fullText, regexMatches){
+								var _filename = '';
+								grunt.file.recurse('js/build/describer-config', callback);
+								function callback(abspath, rootdir, subdir, filename) {
+									var str = filename;
+									var patt = new RegExp(/^main.(.*)/g);
+									var res = patt.test(str);
+									if(res){
+										//grunt.log.write('res: '+_filename);
+										_filename = str;
+									}
+
+								}
+								return "/js/build/describer-config/"+ _filename + "'";
+							}
+						}]
+				}
+			}
+		});
+	});
+
+	// Project configuration.
+	grunt.registerTask('task_step5', function(){
+		grunt.initConfig({
+			imageoptim: {
+				imgcomp: {
+					options: {
+						jpegMini: true,
+						imageAlpha: false,
+						quitAfter: false
+					},
+					src: ['/img/image-teaser-apps-747-119-X2.jpg']
+				}
+			}
+		});
+	});
+
+	grunt.registerTask('task_step6', function(){
+		grunt.initConfig({
+			// WebP configuration
+			webp: {
+				files: {
+					expand: true,
+					//cwd: 'path/to/source/images',
+					/*					src: ['img/add-on-steps/add-on-steps-1.png'],
+										dest: '/'*/
+					src: ['**/*.jpg', '**/*.png', '!**/productimages/*.png'] ,
+					cwd: 'img/',
+					dest: 'img/'
+				},
+				options: {
+					binpath: require('webp-bin').path,
+					preset: 'photo',
+					verbose: true,
+					quality: 80,
+					alphaQuality: 80,
+					compressionMethod: 6,
+					segments: 4,
+					psnr: 50,
+					sns: 50,
+					filterStrength: 40,
+					filterSharpness: 3,
+					simpleFilter: true,
+					partitionLimit: 50,
+					analysisPass: 6,
+					multiThreading: true,
+					lowMemory: false,
+					alphaMethod: 0,
+					alphaFilter: 'best',
+					alphaCleanup: true,
+					noAlpha: false,
+					lossless: false
+				}
+			}
+
+		});
+
+	});
+
+
+
+	grunt.registerTask('compass-task', function(){
+		grunt.initConfig({
+			compass: {                  // Task
+				dev: {                    // Another target
+					options: {
+						sassDir: 'scss',
+						cssDir: 'css'
+					}
+				}
+			}
+		});
+	});
+
+	grunt.registerTask('watch-task', function(){
+		grunt.initConfig({
+			watch: {
+				css: {
+					files: 'scss/**/*.scss',
+					tasks: ['compass-task', 'compass']
+				}
+			}
+		});
+	});
+
+	grunt.registerTask('bake-task', function(){
+		grunt.initConfig( {
+			bake: {
+				build: {
+					files: [ {
+						expand: true,
+						cwd: '../demo-sources/',
+						src: ['**/*.html'],
+						dest: 'demo/',
+						ext: '.html'
+					} ]
+				}
+			}
+		} );
+	});
+
+	grunt.registerTask('watchbake-task', function(){
+		grunt.initConfig( {
+			watch: {
+				bake: {
+					files: '../demo-sources/**',
+					tasks: ['bake-task', 'bake']
+				}
+			}
+		} );
+	});
+
+	grunt.registerTask('sass-task', function(){
+		grunt.initConfig({
+			sass: {
+				options: {
+					//outputStyle: 'expanded'
+					sourceComments: false
+				},
+				dist: {
+					files: [
+						{'.sass-cache/noscript.css': 'scss/noscript.scss'},
+						{'.sass-cache/style.css': 'scss/style.scss'}
+					]
+				}
+			}
+		});
+	});
+
+	grunt.registerTask('autoprefixer-task', function(){
+		grunt.initConfig({
+			autoprefixer: {
+				dist: {
+					files: [
+						{'css/noscript.css': '.sass-cache/noscript.css'},
+						{'css/style.css': '.sass-cache/style.css'}
+					]
+				}
+			}
+		});
+	});
+
+	grunt.registerTask('watchscss-task', function(){
+		grunt.initConfig({
+			watch: {
+				scss: {
+					files: 'scss/**/*.scss',
+					tasks: ['sass-task', 'sass']
+				},
+				css: {
+					files: ['.sass-cache/*.css'],
+					tasks: ['autoprefixer-task', 'autoprefixer']
+				}
+			}
+		});
+	});
+
+
+
+
+
+	grunt.registerTask('default1', ['task_step1', 'clean', 'uglify', 'copy', 'replace', 'cssmin', 'revisions' ]);
+	grunt.registerTask('default2', ['task_step2', 'replace' ]);
+	grunt.registerTask('default3', ['task_step3', 'uglify', 'revisions' ]);
+	grunt.registerTask('default4', ['task_step4', 'replace' ]);
+	grunt.registerTask('default5', ['task_step5', 'imageoptim' ]);
+	grunt.registerTask('default6', ['task_step6', 'webp' ]);
+
+	// SCSS - begin
+	//  mit Compass
+	//grunt.registerTask('watchscss', ['watch-task', 'watch']);
+	// ohne Compass
+	grunt.registerTask('watchscss', ['watchscss-task', 'watch']);
+	// SCSS - end
+
+	grunt.registerTask('watchbake', ['watchbake-task', 'watch']);
+
+
+	//grunt.registerTask('ErstelleMainJSMitVersioniertenJSVerweisen', ['createMainJS', 'replace']);
+	grunt.registerTask('default', ['default1', 'default2', 'default3', 'default4']);
+};
---
+++ js/describer-config/main.js
@@ -0,0 +1,216 @@
+/*!
+ * require-main
+ *
+ * http://joinmedia.de/
+ *
+ * Copyright (c) 2014 join.media GmbH & Co. KG
+ *
+ * Created by aotten77 on 22.08.2014.
+ *
+ * Released under the MIT license
+ */
+var pathToJQuery;
+var buildPath = (window.dc.loadUnminifyVersion) ? '' : 'build/';
+if('querySelector' in document && 'localStorage' in window && 'addEventListener' in window){
+	pathToJQuery = buildPath+'describer-core/libs/jquery-2.1.0';
+}else{
+	pathToJQuery = buildPath+'describer-core/libs/jquery-1.11.0';
+}
+require.config({
+	baseUrl: '/js',
+	paths      : {
+		// ******************* describer-core ******************************
+		'_config'                                           : buildPath+'describer-config/describer',
+		'config.validation'                                 : buildPath+'describer-config/validation',
+		'config.googlemap'                                  : buildPath+'describer-config/googlemap',
+		'config.require-css.owl_carousel'                   : buildPath+'describer-config/require-css/owl.carousel',
+		'config.require-css.video'                          : buildPath+'describer-config/require-css/video',
+		'config.languageTextObj'                            : buildPath+'describer-config/languageTextObj',
+		'language/nls/speak'                                : buildPath+'describer-config/nls/speak',
+		'language/nls/en/speak'                             : buildPath+'describer-config/nls/en/speak',
+
+
+		// ******************* describer-core ******************************
+		'async'                                             : buildPath+'describer-core/libs/async',
+		'jquery'                                            : pathToJQuery,
+		'scrolltotop'                                       : buildPath+'describer-core/libs/scrolltotop',
+		'rAF'                                               : buildPath+'describer-core/libs/rAF',
+
+
+		// mylibs
+		'core'                                              : buildPath+'describer-core/core',
+
+		'_super'                                            : buildPath+'describer-core/_super',
+		'actions.add'                                       : buildPath+'describer-core/actions/add',
+		'actions.ajax'                                      : buildPath+'describer-core/actions/ajax',
+		'actions.apply'                                     : buildPath+'describer-core/actions/apply',
+		'actions.exec'                                      : buildPath+'describer-core/actions/exec',
+		'actions.link'                                      : buildPath+'describer-core/actions/link',
+		'actions.move'                                      : buildPath+'describer-core/actions/move',
+		'actions.remove'                                    : buildPath+'describer-core/actions/remove',
+		'actions.scroll'                                    : buildPath+'describer-core/actions/scroll',
+		'actions.set'                                       : buildPath+'describer-core/actions/set',
+		/*		'actions.sticky'                                   : 'mylibs/actions/sticky',*/
+		'actions.toggle'                                    : buildPath+'describer-core/actions/toggle',
+		'actions.trigger'                                   : buildPath+'describer-core/actions/trigger',
+
+
+
+		// ******************* describer-more ******************************
+
+		'modules.carousel'                                  : buildPath+'describer-add-ons/modules/carousel',
+		//'modules.carousel-ts'                               : buildPath+'describer-more/modules/carousel-ts',
+		//
+		///*'modules.carousel-ts'                              : 'describer-more/empty',*/
+		//'modules.countdown'                                 : buildPath+'describer-more/modules/countdown',
+		//'modules.dependentSelectionGroup'                   : buildPath+'describer-more/modules/dependentSelectionGroup',
+		//'modules.equalheights'                              : buildPath+'describer-more/modules/equalheights',
+		//'modules.form.autocomplete'                         : buildPath+'describer-more/modules/form/autocomplete',
+		//'modules.form.formvalidate'                         : buildPath+'describer-more/modules/form/formvalidate',
+		//'modules.form.selectOptionExtractInGroup'           : buildPath+'describer-more/modules/form/selectOptionExtractInGroup',
+		//'modules.form.submitbutton'                         : buildPath+'describer-more/modules/form/submitbutton',
+		//'modules.fotoupload'                                : buildPath+'describer-more/modules/fotoupload',
+		'modules.googlemap'                                 : buildPath+'describer-add-ons/modules/googlemap',
+		'modules.modal'                                     : buildPath+'describer-add-ons/modules/modal',
+		'modules.onView'                                    : buildPath+'describer-add-ons/modules/onView',
+		//'modules.parallax'                                  : buildPath+'describer-more/modules/parallax',
+		//'modules.scrollControlFrames'                       : buildPath+'describer-more/modules/scrollControlFrames',
+		//'modules.scrollControlTransform'                    : buildPath+'describer-more/modules/scrollControlTransform',
+		//'modules.tablesort'                                 : buildPath+'describer-more/modules/tablesort',
+		//'modules.video'                                     : buildPath+'describer-more/modules/video',
+		//'modules.videocontrol'                              : buildPath+'describer-more/modules/videoControl',
+		//'modules.videoplayer'                               : buildPath+'describer-more/modules/videoplayer',
+
+		// untouched externel
+		'i18n'                                              : buildPath+'describer-add-ons/modules/i18n',
+		//'jquery_canvasResize'                               : buildPath+'describer-more/plugins/jquery.canvasResize',
+		//'fastclick'                                         : buildPath+'describer-more/plugins/fastclick',
+		//'fixedfixed'                                        : buildPath+'describer-more/plugins/fixedfixed',
+		//'fixedsticky'                                       : buildPath+'describer-more/plugins/fixedsticky',
+		//'jquery_mousewheel'                                 : buildPath+'describer-more/plugins/jquery.mousewheel',
+		'md5'                                               : buildPath+'describer-add-ons/plugins/md5',
+		//'video_4_10_2'                                      : buildPath+'describer-more/plugins/video.4.10.2',
+		//'videojs-youtube'                                   : buildPath+'describer-more/plugins/youtube',
+
+		// customized externel
+		//'jquery_autocomplete'                               : buildPath+'describer-more/plugins/customized/jquery.autocomplete',
+		'jquery_ba-dotimeout'                               : buildPath+'describer-core/libs/jquery.ba-dotimeout',
+		//'jquery_countdown'                                  : buildPath+'describer-more/plugins/customized/jquery.countdown',
+		//'jquery_countdown-de'                               : buildPath+'describer-more/plugins/customized/jquery.countdown-de',
+		//'jquery_exif'                                       : buildPath+'describer-more/plugins/customized/jquery.exif',
+		//'jquery_parallax'                                   : buildPath+'describer-more/plugins/customized/jquery.parallax',
+		//'jquery_plugin'                                     : buildPath+'describer-more/plugins/customized/jquery.plugin',
+		//'jquery_smoothMousewheel'                           : buildPath+'describer-more/plugins/customized/jquery.smoothMousewheel',
+		//'jquery_validate'                                   : buildPath+'describer-more/plugins/customized/jquery.validate',
+		'owl_carousel'                                      : buildPath+'describer-add-ons/plugins/customized/owl.carousel',
+		//'video'                                             : buildPath+'describer-more/plugins/customized/video',
+
+		// overwritings externel
+		//'overwritings.jquery_validate'                      : buildPath+'describer-more/plugins/overwritings/jquery.validate',
+		'overwritings.owl_carousel'                         : buildPath+'describer-add-ons/plugins/overwritings/owl.carousel'
+	},
+	shim: {
+		//'jquery_canvasResize': {
+		//	deps: ['jquery_exif', 'jquery']
+		//},
+		//'fastclick': {
+		//
+		//},
+		//'fixedsticky': {
+		//	deps: ['fixedfixed']
+		//}
+	},
+	map: {
+	    '*': {
+	        'css': buildPath+'describer-add-ons/plugins/css' // or whatever the path to require-css is
+	    }
+	},
+	waitSeconds: 500,
+	locale: document.getElementsByTagName('html')[0].getAttribute('lang')
+});
+
+
+require(['jquery', '_config'], function require_jquery($){
+	// need jquery
+	require([ 'core', 'jquery_ba-dotimeout' ], function require_helpers(){
+		// DomReady
+		$(function domReady(){
+			var $body = $('body');
+
+			// -----------------------------------------------------
+			// --------------- User Events -------------------------
+			// -----------------------------------------------------
+
+			//---------------- Listener for focus, change, blur, checkValidation -------------------------------------
+
+
+			// Jedes Input(ausgenommen type="submit"), texarea und select feuert ein Event "checkValidation" wenn focus, change oder blur getriggert wird.
+			// TODO Andreas evtl. obsolate da hier auch mit jmtrigger:checkValidation gearbeitet werden kann. Prüfen. Evtl. dominit="true" und blur, focus, jmtrigger:checkValidtion in den Config-Event-Stirng
+			$body.on(   'blur ' +
+						'change ' +
+						'focus ' +
+						'checkValidation', 'form[data-jmname="form"]', jmHF.eventDelegationTrigger);
+
+			// Change-Listener für select, textarea und input ohne input[type="radio"] zur initialisierung und Aufruf der change-Funktion des Plugins
+			$body.on('change',  'select[data-jmname],' +
+								'textarea[data-jmname],'+
+								'input[data-jmname]:not(input[type="radio"][data-jmname])', jmHF.eventDelegationTrigger);
+
+			// Change-Listenerinput[type="radio"]
+			$body.on('change',  'input[type="radio"][data-jmname]', jmHF.eventDelegationTriggerForRadios);
+
+
+			//---------------- Listener for click --------------------------------------------------------
+
+			$body.on('click', '[data-jmname]:not(label[data-jmname], a[data-jmname], form[data-jmname])', jmHF.eventDelegationTrigger);
+
+			$body.on('click', 'a[data-jmname]', jmHF.eventDelegationTriggerForATags);
+
+			$body.on('click', 'label[data-jmname]', jmHF.eventDelegationTriggerForLabels);
+
+
+			// -----------------------------------------------------
+			// --------------- generische Events -------------------
+			// -----------------------------------------------------
+
+
+			$body.on('dominit', '[data-jmname]', jmHF.eventDelegationTriggerForDomInit);
+
+			// Change-Listener für select, input[type="radio"] und input[type="checkbox"] zur initialisierung und Aufruf der change-Funktion des Plugins
+			$body.on('jmtrigger', '[data-jmname]', jmHF.eventDelegationTrigger);
+
+
+			$(window).on('resize', jmHF.checkOrientation);
+			// -----------------------------------------------------
+			// -------- ausführen von Domready-Funktionen ----------
+			// -----------------------------------------------------
+
+			// läde die auf der Seite benötigten JS-Module
+			$body.requirementsForJmPlugins();
+
+			// Initialisiert die Elemente, die mit dem Attribut data-jmdominit="true" versehen sind
+			$body.triggerSelfexecObj();
+
+			// Externe Lib. Sie wird verwendet, um ein delay von 300ms bei Klick auf Touch-Devices zu unterdrücken.
+			// Die Verzögerung entsteht durch das warte des Device auf einen evtl. doppleClick, der ein Zoomen in die Seite auslöst.
+			// Touchstart -> Klick
+			//FastClick.attach(document.body);
+
+			// Führt die im DomReadyObject hinterlegten Funktionen aus
+			execDomReadyObject();
+
+			// Trigger jmtrigger:hash on element with id === hash
+			if(window.location.hash !== ''){
+				$(window.location.hash).jmtrigger('hash');
+			}
+
+			// Trigger Picturefill um die entsprechenden Images in die Div-Container zu injecten
+			try{picturefill();}catch(e){}
+
+			if(dc.debug){
+				jmHF.checkConfigJS();
+				jmHF.checkJmNameElementenOnNecessaryDominitAttribut();
+			}
+		});
+	});
+});
